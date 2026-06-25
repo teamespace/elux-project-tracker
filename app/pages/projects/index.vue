923feeb703a45a5fbcd27f87ebd1f5032b72095b
Rasya Ardiansyah <rasya@queebo.chat>
2026-06-26 01:20:41 +0700

    fix: remove black New project button from projects page, restore it in + New dropdown

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -145,10 +145,6 @@ const progIcon = (p: Proj) => p.status === 'on-track' && p.progress >= 50
             List
           </button>
         </div>
-        <button class="btn-new-proj">
-          <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M12 5v14M5 12h14"/></svg>
-          New project
-        </button>
       </div>
     </div>
 
